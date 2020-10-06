ad62d2c7aa2510aea820d183c3dca95a73e24c14
Diego Fincatto <diego@fincatto.com>
2020-10-06 00:51:22 -0300

    Exemplo funciona de pub e sub.

--- src/main/java/com/fincatto/springbootgcp/SpringBootGcpApplication.java
+++ src/main/java/com/fincatto/springbootgcp/SpringBootGcpApplication.java
@@ -2,10 +2,13 @@ package com.fincatto.springbootgcp;
 
 import com.google.api.core.ApiFuture;
 import com.google.api.gax.paging.Page;
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.cloud.pubsub.v1.Publisher;
+import com.google.cloud.pubsub.v1.Subscriber;
 import com.google.cloud.storage.*;
 import com.google.protobuf.ByteString;
-import com.google.protobuf.Descriptors;
+import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
 import org.slf4j.Logger;
@@ -15,7 +18,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -33,13 +35,31 @@ public class SpringBootGcpApplication {
     }
 
     private static void testePubSub() throws InterruptedException, ExecutionException, IOException, TimeoutException {
-        final TopicName topicName = TopicName.of("dfteste", "test-topic");
-        testePubSubPubish(topicName);
+        testePubSubPubish(TopicName.of("dfteste", "test-topic"));
+        testePubSubReceive(ProjectSubscriptionName.of("dfteste", "teste-pull"));
+    }
 
+    private static void testePubSubReceive(ProjectSubscriptionName subscriptionName) {
+        // Instantiate an asynchronous message receiver.
+        MessageReceiver receiver =
+                (PubsubMessage message, AckReplyConsumer consumer) -> {
+                    LOGGER.info("S[{}] {}", message.getMessageId(), message.getData().toStringUtf8());
+                    consumer.ack();
+                };
+
+        Subscriber subscriber = null;
+        try {
+            subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
+            subscriber.startAsync().awaitRunning();
+            LOGGER.info("Listening for messages on '{}':", subscriptionName.toString());
+            subscriber.awaitTerminated(30, TimeUnit.SECONDS);
+        } catch (TimeoutException timeoutException) {
+            subscriber.stopAsync();
+        }
     }
 
     private static void testePubSubPubish(TopicName topicName) throws InterruptedException, ExecutionException, TimeoutException, IOException {
-       Publisher publisher = null;
+        Publisher publisher = null;
         try {
             publisher = Publisher.newBuilder(topicName).build();
 
@@ -47,7 +67,7 @@ public class SpringBootGcpApplication {
             final PubsubMessage pubsubMessage = PubsubMessage.newBuilder().putAttributes("att1", "v1").setData(data).build();
             final ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
             final String messageId = messageIdFuture.get(15, TimeUnit.SECONDS);
-            LOGGER.info("Published message ID: " + messageId);
+            LOGGER.info("P[{}] {} ", messageId, data.toStringUtf8());
         } finally {
             if (publisher != null) {
                 publisher.shutdown();
